0e897f952028eb34c7bd99a7a95ca5bbe006a625
yusuf-ozbay <ozbay9985@gmail.com>
2025-06-18 15:47:24 +0300

    first commit

---
+++ src/main/java/org/example/bookkeepio/BookkeepioApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookkeepio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeepioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkeepioApplication.class, args);
+	}
+
+}
